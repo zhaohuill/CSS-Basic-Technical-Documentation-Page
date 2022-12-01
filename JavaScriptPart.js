c818fef1c5a3c90ed6679767c35e5879157ff496
zhao <zhaohuili0210@gmail.com>
2022-11-30 19:09:38 -0500

    Initial Commit

---
+++ JavaScriptPart.js
@@ -0,0 +1,2 @@
+const projectName = 'css-basic-labels-technical-documents-page';
+localStorage.setItem('example_project', 'Technical Docs Page');
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSS-Basic-Technical-Documentation-Page
+4th of 5 Projects for the Responsive Web Design Certificate of freeCodeCamp.
